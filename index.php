039f1f44e106ae38149f57c437a851fa6c842a11
dodo0211 <79956201+dodo0211@users.noreply.github.com>
2021-04-11 16:42:51 +0900

    basic layout

---
+++ index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>초록리싸이클</header>
+
+    <div>
+        <section>이미지 슬라이드</section>            
+    </div>
+
+    <nav>
+        <ul>
+            <li>가정용</li>
+            <li>사무용</li>
+            <li>인테리어</li>
+            <li>주방</li>
+            <li>수집</li>
+            <li>산업</li>
+            <li>의류</li>
+        </ul>
+    </nav>
+
+    <main>
+        <span>
+            <section>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>                
+            </section>
+            <section>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>                
+            </section>
+            <section>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>                
+            </section>
+            <section>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>
+                <span>
+                    <p>사진</p>
+                    <p>제품명</p>
+                    <p>가격</p>
+                </span>                
+            </section>
+        </span>
+        
+        <aside>
+            <div>로그인</div>
+            <div>
+                <p>문의번호</p>
+                <p>계좌번호</p>
+                <p>주소</p>
+            </div>            
+            <div>약도</div>
+        </aside>        
+    </main>
+
+    <footer>
+        <div>이용약관</div>
+        <div>사업자정보</div>
+    </footer>
+    
+</body>
+</html>
