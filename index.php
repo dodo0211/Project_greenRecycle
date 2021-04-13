dc02bbc2b9bbd306200b815c76d244c7ed472a64
dodo0211 <79956201+dodo0211@users.noreply.github.com>
2021-04-13 20:32:05 +0900

    nav dropdown menu 구현

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/main.css?after3">
+    <link rel="stylesheet" href="./css/main.css?after1">
     <title>초록리싸이클</title>
 </head>
 <body>
@@ -21,13 +21,76 @@
             <section>이미지 슬라이드</section>            
         </div>
         <ul>
-            <li>가정용</li>
-            <li>사무용</li>
-            <li>인테리어</li>
-            <li>주방</li>
-            <li>수집</li>
-            <li>산업</li>
-            <li>의류</li>
+            <li>
+                가정용
+                <ul class="dropdown-content">
+                    <a href="#">가구</a>
+                    <a href="#">가전</a>
+                </ul>
+            </li>
+            <li>
+                사무용
+                <ul class="dropdown-content">
+                    <a href="#">가구</a>
+                    <a href="#">사무용품</a>
+                    <a href="#">의자</a>
+                    <a href="#">쇼파</a>
+                    <a href="#">금고</a>
+                    <a href="#">TV</a>
+                    <a href="#">모니터</a>
+                </ul>
+            </li>
+            <li>
+                인테리어
+                <ul class="dropdown-content">
+                    <a href="#">소품</a>
+                </ul>
+            </li>
+            <li>
+                주방
+                <ul class="dropdown-content">
+                    <a href="#">주방기기</a>
+                    <a href="#">주방용품</a>
+                    <a href="#">식기_업소용</a>
+                    <a href="#">식기_가정용</a>
+                    <a href="#">유기 그릇</a>
+                </ul>
+            </li>
+            <li>
+                수집
+                <ul class="dropdown-content">
+                    <a href="#">수집</a>
+                </ul>
+            </li>
+            <li>
+                산업
+                <ul class="dropdown-content">
+                    <a href="#">산업용품</a>
+                    <a href="#">산업자재</a>
+                </ul>
+            </li>
+            <li>
+                의류
+                <ul class="dropdown-content">
+                    <a href="#">잡화</a>
+                    <a href="#">가방</a>
+                    <a href="#">신발</a>
+                    <a href="#">의류</a>
+                </ul>
+            </li>
+            <li>
+                그림
+                <ul class="dropdown-content">
+                    <a href="#">동양화</a>
+                    <a href="#">서양화</a>
+                </ul>
+            </li>
+            <li>
+                건축자재
+                <ul class="dropdown-content">
+                    <a href="#">건축자재</a>
+                </ul>
+            </li>
         </ul>
     </nav>
 
