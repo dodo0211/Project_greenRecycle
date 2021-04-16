a36fca509e9b073d8e955fff054cd0e988de1878
dodo0211 <79956201+dodo0211@users.noreply.github.com>
2021-04-16 21:51:15 +0900

    footer
    footer 구현 완료

--- common/lib/footer.php
+++ common/lib/footer.php
@@ -1,2 +1,21 @@
-<div>이용약관</div>
-<div>사업자정보</div>
+<span>
+    <div>
+        <div>법인명(상호): 초록리싸이클</div>
+        <div>사업자정보</div>
+        <div>사업자등록정보 [.....]</div>
+        <div>사업자정보확인</div>
+    </div>
+
+    <div>
+        <div>대표전화 010-6202-7966</div>
+        <div>경기도 구리시 아차산로 256 초록마을</div>
+    </div>
+    
+    <div>개인정보보호책임자: 김성제(ace968301@naver.com)</div>
+
+</span>
+
+
+
+
+<span>mark</span>
