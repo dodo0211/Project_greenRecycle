1b492d0b1410bbed38de907e30091a5af1f8ccff
sensebecommon <79956201+dodo0211@users.noreply.github.com>
2021-04-19 18:13:28 +0900

    nav 이미지 슬라이드
    구현 중

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,29 +10,32 @@
     <script src="https://kit.fontawesome.com/e5f544887c.js" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@700&display=swap" rel="stylesheet">
+    <script src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/Project_greenRecycle/common/js/slide.js"></script>
     <title>초록리싸이클</title>
 </head>
-<body>
+
+<body onload="slide_func()">
     <header>
-        <?php include $_SERVER['DOCUMENT_ROOT']."/Project_greenRecycle/common/lib/header.php";?>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/Project_greenRecycle/common/lib/header.php"; ?>
     </header>
 
     <nav>
-        <?php include $_SERVER['DOCUMENT_ROOT']."/Project_greenRecycle/common/lib/nav.php";?>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/Project_greenRecycle/common/lib/nav.php"; ?>
     </nav>
 
     <main>
         <span>
-            <?php include $_SERVER['DOCUMENT_ROOT']."/Project_greenRecycle/common/lib/main.php";?>  
+            <?php include $_SERVER['DOCUMENT_ROOT'] . "/Project_greenRecycle/common/lib/main.php"; ?>
         </span>
         <aside>
-            <?php include $_SERVER['DOCUMENT_ROOT']."/Project_greenRecycle/common/lib/aside.php";?>
+            <?php include $_SERVER['DOCUMENT_ROOT'] . "/Project_greenRecycle/common/lib/aside.php"; ?>
         </aside>
     </main>
 
     <footer>
-        <?php include $_SERVER['DOCUMENT_ROOT']."/Project_greenRecycle/common/lib/footer.php";?>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . "/Project_greenRecycle/common/lib/footer.php"; ?>
     </footer>
-    
+
 </body>
+
 </html>
