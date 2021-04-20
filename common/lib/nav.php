d63d56fa41b6280211688d26b5c2f316362802db
dodo0211 <freean2468@gmail.com>
2021-04-20 16:52:03 +0900

    이미지 슬라이드 변경 중

--- common/lib/nav.php
+++ common/lib/nav.php
@@ -14,6 +14,24 @@
                 <a href="#" class="next">next</a>
             </div>
         </div>
+        <!-- <div class='mainArea'>
+            <div class='post photoSet 1photos'>
+                <div class='slideshow'>
+                    <div class='slide'>
+                        <img src='https://i.imgur.com/76nykRK.gif' />
+                    </div>
+                    <div class='slide'>
+                        <img src='https://i.imgur.com/x4nn8FD.jpg' />
+                    </div>
+                    <div class='slide'>
+                        <img src='https://i.imgur.com/rPgXc03.jpg' />
+                    </div>
+                    <div class='slide'>
+                        <img src='https://i.imgur.com/bUBp4t0.jpg' />
+                    </div>
+                </div>
+            </div>
+        </div> -->
     </section>
 </div>
 <ul>
