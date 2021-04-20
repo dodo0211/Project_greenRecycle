fe5524c6807bf63578134439175a30029e699b2d
dodo0211 <freean2468@gmail.com>
2021-04-20 15:54:30 +0900

    네이버 지도 api -> 지도 이미지로 변경

--- common/lib/aside.php
+++ common/lib/aside.php
@@ -1,19 +1,26 @@
 <div>로그인</div>
 <div>
     <p>010-6202-7966</p>
-    <p>계좌번호</p>
-    <p>위치</p>
-</div>            
+</div>       
 <div>
+    <br/>
+    <p>계좌번호</p>
+</div>     
+<div>
+    <br/>
+    <p>위치</p>
     <p>경기도 구리시 아차산로256 초록리싸이클</p>
-    <div id="map" style="width:100%;height:300px;"></div>
+    <!-- <div id="map" style="width:100%;height:300px;"></div> -->
+</div>
+<div>
+    <img id="map" src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_greenrecycle/image/map.png" alt="map image">
 </div>
 
-<script>
+<!-- <script>
     let mapOptions = {
         center: new naver.maps.LatLng(37.577413, 127.128646),
         zoom: 18
     };
 
     let map = new naver.maps.Map('map', mapOptions);
-</script>
+</script> -->
--- common/lib/head.php
+++ common/lib/head.php
@@ -4,7 +4,7 @@
 <meta name="referrer" content="no-referrer">
 
 <!-- common css -->
-<link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/Project_greenRecycle/style/main.css?after=30">
+<link rel="stylesheet" href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/Project_greenRecycle/style/main.css?after=31">
 
 <!-- font awesome -->
 <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -17,6 +17,6 @@
 <script src="http://<?php echo $_SERVER['HTTP_HOST']; ?>/Project_greenRecycle/common/js/slide.js" defer></script>
 
 <!-- naver map api -->
-<script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=h8g83wxwiu"></script>
+<!-- <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=h8g83wxwiu"></script> -->
 
 <title>초록리싸이클</title>
