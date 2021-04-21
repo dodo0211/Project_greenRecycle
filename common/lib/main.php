44cc8fba38a8b33059f9cecb5aa7ecc2abac27a8
dodo0211 <freean2468@gmail.com>
2021-04-21 12:19:10 +0900

    legacy 코드 삭제 및 레이아웃 height 100% 적용

--- common/lib/main.php
+++ common/lib/main.php
@@ -1,48 +1,52 @@
 <?php
     const COLUMN_MAX = 4;
 
-    foreach ($category_array as $value) {
-        $sql = "SELECT * FROM product_table WHERE category = '$value' ORDER By date ASC LIMIT 5";
-        $result = $conn->query($sql);
-        ?>
-        <section>
-        <?php
-            if ($result->num_rows > 0) {
-                for ($i = 0; $i < COLUMN_MAX; $i += 1) {
-                    ?>
-                    <span>
-                    
-                    <?php
-                        $row = $result->fetch_assoc();
-                        if (isset($row)) {
-                            $price = number_format($row["price"], 0, '', ',')."원";
-                            $link = $row["link"];
-                            $name = $row["name"];
-                            $category = $row["category"];
-                            $id = $row["id"];
-                            ?>
-                            <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_greenrecycle/product/product.php?id=<?php echo $id;?>&category=<?php echo $category;?>">
-                                <img src="<?php echo $link ?>"/>
-                            </a>
-                            <p><?php echo $name ?></p>
-                            <p><?php echo $price ?></p>
-                            <?php
-                        } else {
-                            ?>
-                            <a>
-                                <img style="opacity:0;">
-                            </a>
-                            <p></p>
-                            <p></p>
-                            <?php
-                        }
-                    ?>
-                    </span>
-                    <?php
+    foreach (array_keys($division_array) as $first_division) {
+        foreach ($division_array[$first_division] as $second_division) {
+            $second_division_text = array_search($second_division, $division_array[$first_division]);
+            $sql = "SELECT * FROM product_table WHERE category = '$second_division' ORDER By date ASC LIMIT 5";
+            $result = $conn->query($sql);
+            echo $second_division_text;
+            ?>
+            <section>
+            <?php
+                if ($result->num_rows > 0) {
+                    for ($i = 0; $i < COLUMN_MAX; $i += 1) {
+                        ?>
+                        <span>
+                        
+                        <?php
+                            $row = $result->fetch_assoc();
+                            if (isset($row)) {
+                                $price = number_format($row["price"], 0, '', ',')."원";
+                                $link = $row["link"];
+                                $name = $row["name"];
+                                $category = $row["category"];
+                                $id = $row["id"];
+                                ?>
+                                <a href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/project_greenrecycle/product/product.php?id=<?php echo $id;?>&category=<?php echo $category;?>">
+                                    <img src="<?php echo $link ?>"/>
+                                </a>
+                                <p><?php echo $name ?></p>
+                                <p><?php echo $price ?></p>
+                                <?php
+                            } else {
+                                ?>
+                                <a>
+                                    <img style="opacity:0;">
+                                </a>
+                                <p></p>
+                                <p></p>
+                                <?php
+                            }
+                        ?>
+                        </span>
+                        <?php
+                    }
                 }
-            }
-        ?>
-        </section>
-        <?php
+            ?>
+            </section>
+            <?php
+        }
     }
 ?>
